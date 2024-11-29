ceb4adf12e5f24c9cbc8e7a3751c1d9465cbf83c
Luomis96 <vluomaranta@gmail.com>
2024-11-29 16:19:51 +0200

    Taskin luominen
    task

---
+++ backend/controllers/tasks.js
@@ -0,0 +1,35 @@
+const tasksRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+const Task = require('../models/task')
+const User = require('../models/user')
+
+
+tasksRouter.get('/', async (request, response) => {
+    const task = await Task.find({}).populate('user', { username: 1, name: 1 })
+    response.json(tasks)
+  })
+
+  
+  tasksRouter.post('/', async (request, response) => {
+    const body = request.body
+  
+    const decodedToken = jwt.verify(request.token, process.env.SECRET) 
+  
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    const newtask = new Task({
+      title: body.title,
+      user: user.id
+    })
+    const savedTask= await newtask.save()
+    user.tasks = user.tasks.concat(savedTasks.id)
+    await user.save()
+
+    response.status(201).json(savedTask)
+  })
+
+  module.exports = tasksRouter
+  
---
+++ backend/models/task.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose") 
+
+const taskSchema = mongoose.Schema({
+
+    title:{
+    type: String,
+    required:true,
+    },
+    user:[{
+        type: mongoose, Schema, Types, ObjectId,
+        ref: 'User'
+    }]
+    
+})
+
+taskSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+  
+  module.exports = mongoose.model('Task', taskSchema)
